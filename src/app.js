8ec4125f4f0e214099a8d1b12189e4230a568da6
Timofey Biryukov <timofeybiryukov@gmail.com>
2018-10-25 17:00:50 +0600

    app fix

--- src/app.js
+++ src/app.js
@@ -3,14 +3,14 @@
 
 const Nodal = require('nodal');
 // const applicationFactory = Nodal.require('lib/core/application-factory.js'); // TODO
-const applicationFactory = (() => {
-  module.exports = function() {
+const applicationFactory = function() {
+  module.exports = function () {
     if (!global.__app) {
       global.__app = new Nodal.ApplicationExtended();
     }
     return global.__app;
   };
-})();
+};
 
 let app = applicationFactory();
 app.listen(Nodal.my.Config.secrets.port);
